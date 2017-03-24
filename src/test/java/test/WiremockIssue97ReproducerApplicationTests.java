b709d2102302fc25747f7e31f60760a91e03f1d9
slaurent <slaurent@apache.org>
2017-03-24 11:02:30 +0100

    simplified test

--- src/test/java/test/WiremockIssue97ReproducerApplicationTests.java
+++ src/test/java/test/WiremockIssue97ReproducerApplicationTests.java
@@ -2,7 +2,6 @@ package test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
@@ -11,7 +10,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.annotation.Repeat;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -21,31 +19,24 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WiremockIssue97ReproducerApplicationTests {
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(8089);
-    
-    private RestTemplate restTemplate = new RestTemplate();
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(8089);
 
-    @Before
-    public void setupWireMock() {
-        stubFor(//
-                get(urlEqualTo("/test"))//
-                        .willReturn(aResponse().withStatus(200)//
-                                .withHeader("Content-Type", "application/json")//
-                                .withBody("{ \"hello\": \"get\" }")));
-        stubFor(//
-                post(urlEqualTo("/test"))//
-                        .willReturn(aResponse().withStatus(200)//
-                                .withHeader("Content-Type", "application/json")//
-                                .withFixedDelay(0)//
-                                .withBody("{ \"hello\": \"post\" }")));
+	private RestTemplate restTemplate = new RestTemplate();
 
-    }
+	@Before
+	public void setupWireMock() {
+		stubFor(//
+				get(urlEqualTo("/test"))//
+						.willReturn(aResponse().withStatus(200)//
+								.withHeader("Content-Type", "application/json")//
+								.withBody("{ \"hello\": \"get\" }")));
+	}
 
-    @Test
-    @Repeat(100)
-    public void test() {
-        restTemplate.getForObject("http://localhost:8089/test", String.class);
-    }
+	@Test
+	@Repeat(1000)
+	public void test() {
+		restTemplate.getForObject("http://localhost:8089/test", String.class);
+	}
 
 }
